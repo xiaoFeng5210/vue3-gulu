d0c8c84ccc2b39ded859101d15c590e8b3d5e546
xiaofeng <804875728@qq.com>
2020-08-02 16:27:37 +0800

    router初始化以及搭建html结构

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createWebHashHistory, createRouter } from "vue-router";
+import "./index.css";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+
+const history = createWebHashHistory();
+const router = createRouter({
+  history: history,
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/doc",
+      component: Doc,
+    },
+  ],
+});
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>轱辘</h1>
+      <h2>一个好用的vue3 UI框架</h2>
+      <a>GitHub</a>
+      <a>开始</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
