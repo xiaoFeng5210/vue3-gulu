153fc1551ff6f8f8209341b0def4d277df883109
xiaofeng <804875728@qq.com>
2020-08-02 20:47:59 +0800

    switch组件样式完成

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>switch组件</div>
+  <div class="g__box">
+    <div class="show__switch">
+      <button>
+        <span></span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 export default {};
 </script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: blue;
+  border-radius: $h / 2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
+</style>
