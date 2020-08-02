1bc659c2fcb497068d5773031a1cb2206590c994
xiaofeng <804875728@qq.com>
2020-08-02 21:11:40 +0800

    switch组件单独封装

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,35 +1,24 @@
 <template>
   <div class="g__box">
     <div class="show__switch">
-      <button>
-        <span></span>
-      </button>
+      <SwitchComponent />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import SwitchComponent from "../lib/SwitchComponent.vue";
+export default {
+  components: {
+    SwitchComponent
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-$h: 22px;
-$h2: $h - 4px;
-button {
-  height: $h;
-  width: $h * 2;
-  border: none;
-  background: blue;
-  border-radius: $h / 2;
-  position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
+.g__box {
+  .show__switch {
+    width: 20%;
+  }
 }
 </style>
