693ca2dfffff52ad77a41dc7f3a4024a6e2b0216
xiaoFeng <804875728@qq.com>
2021-12-30 22:31:55 +0800

    添加返回首页

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="topnav">
     <div class="logo" @click="handleChangeMenu" style="padding-left: 50px;">logo</div>
-    <div class="menu" style="padding-right: 50px;">menu</div>
+    <div class="menu" style="padding-right: 50px;">
+      <router-link to="/" style="text-decoration: none; color: black">首页</router-link>
+    </div>
   </div>
 </template>
 
