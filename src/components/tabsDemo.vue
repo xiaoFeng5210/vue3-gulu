4fde672ead2ae3d37c257ce2ac2c402af2294574
xiaoFeng <804875728@qq.com>
2021-12-30 14:54:31 +0800

    切换tab

--- src/components/tabsDemo.vue
+++ src/components/tabsDemo.vue
@@ -1,9 +1,11 @@
 <template>
     <h2>tabs组件示例</h2>
-    <Tabs>
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航2">内容2</Tab>
-    </Tabs>
+    <div style="width: 400px;margin-left: 50%; transform: translateX(-50%);margin-top: 10px">
+        <Tabs selected="导航1">
+            <Tab title="导航1">内容1</Tab>
+            <Tab title="导航2">内容2</Tab>
+        </Tabs>
+    </div>
 </template>
 
 <script lang="ts">
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,24 @@
 <template>
-    <div v-for="t in titles" :key="t">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :key="index" :is="c" />
+    <div class="gulu-tabs-nav">
+        <div
+            class="gulu-tabs-nav-item"
+            :class="{ selected: t === selected }"
+            v-for="t in titles"
+            :key="t"
+        >{{ t }}</div>
+    </div>
+    <div>
+        <!-- <component v-for="(c, index) in defaults" :key="index" :is="c" /> -->
+        <component :is="current"></component>
+    </div>
 </template>
 
 <script>
 import Tab from './Tab.vue'
 export default {
+    props: {
+        selected: String
+    },
     setup(props, context) {
         const defaults = context.slots.default();
         defaults.forEach((tag) => {
@@ -14,7 +27,24 @@ export default {
             }
         });
         const titles = defaults.map(tag => tag.props.title);
-        return { defaults, titles }
+        const current = defaults.filter(tag => tag.props.title === props.selected)[0];
+        return { defaults, titles, current }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.gulu-tabs-nav {
+    display: flex;
+    border-bottom: 1px solid #d7dbdb;
+}
+.gulu-tabs-nav-item {
+    padding-bottom: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    cursor: pointer;
+}
+.selected {
+    color: #609fea;
+}
+</style>
