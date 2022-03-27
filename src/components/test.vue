dead6063136ab79fa615706d300b3104dd0580bf
Eadweard <804875728@qq.com>
2022-03-27 16:14:10 +0800

    grid布局测试

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="g_box">
-    <div></div>
+    <div v-for="item in 15" :key="item" class="box_item"></div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style scoped lang="scss">
 .g_box {
   display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  gap: 20px;
+  justify-content: center;
+  .box_item {
+    width: 100%;
+    height: 200px;
+    border: 1px solid gray;
+  }
 }
 </style>
